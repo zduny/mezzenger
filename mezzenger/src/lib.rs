8a7af7778b78f608e347dbb6006c586c66fa06e9
Daniel Zduniak <dzduniak@gmail.com>
2022-10-28 09:26:46 +0200

    Clippy.

--- mezzenger/src/lib.rs
+++ mezzenger/src/lib.rs
@@ -27,7 +27,7 @@ where
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Closed => write!(f, "transport closed"),
-            Self::Other(other) => write!(f, "{}", other),
+            Self::Other(other) => write!(f, "{other}"),
         }
     }
 }
