d9e273079fb61bd3d8721697ac859f3527f31502
Daniel Zduniak <dzduniak@gmail.com>
2022-11-21 12:30:52 +0100

    Update doc.

--- mezzenger-websocket/src/lib.rs
+++ mezzenger-websocket/src/lib.rs
@@ -6,10 +6,6 @@
 //! - native applications (through [tokio-tungstenite](https://github.com/snapview/tokio-tungstenite)),
 //! - [warp](https://github.com/seanmonstar/warp) servers (enabled with `warp` feature).
 //!
-//! **NOTE**: interface may vary depending on the target platform:
-//! For example WASM targets have a `Transport` struct that implements both `Sink` and `Stream`,
-//! while on native targets they're separated into `Sender` and `Receiver` structs.
-//!
 //! See [repository](https://github.com/zduny/mezzenger) for more info.
 
 #[cfg(all(feature = "warp", not(target_arch = "wasm32")))]
