449292a2c6962ff85713900c86ed88d59645f8a0
Daniel Zduniak <dzduniak@gmail.com>
2023-09-19 22:00:38 +0200

    Update run_ci.sh

--- mezzenger-tests/run_ci.sh
+++ mezzenger-tests/run_ci.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-sleep 1 && chrome --headless http://localhost:3030 &
+sleep 1 && chrome --headless --enable-logging http://localhost:3030 &
 sleep 1 && ./tests_client &
 ./tests_server
