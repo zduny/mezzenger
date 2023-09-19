066ef4edeeb2bb44f0ae0765f09538bcaf404d7c
Daniel Zduniak <dzduniak@gmail.com>
2023-09-19 22:33:43 +0200

    Update run_cli.sh.

--- mezzenger-tests/run_ci.sh
+++ mezzenger-tests/run_ci.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-sleep 1 && chrome --headless --enable-logging=stderr --v=1  http://localhost:3030 &
+sleep 1 && chrome --headless --enable-logging=stderr --v=1 --disable-gpu http://localhost:3030 &
 sleep 1 && ./tests_client &
 ./tests_server
